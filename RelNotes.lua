de05cc9079bfc8039de125103788d72c1fe4dc1b
Alar of Runetotem <alar@aspide.it>
2026-01-05 23:17:50 +0100

    Version 1.18.0

--- RelNotes.lua
+++ RelNotes.lua
@@ -17,6 +17,9 @@ ones not your armor class.
 Note, I recommend OneChoice to improve you quest reward selection experience
 ]])
 self:HF_Paragraph('Release Notes')
+self:RelNotes(1,18,0,[[
+Toc: 11.2.7
+]])
 self:RelNotes(1,17,2,[[
 Fix: Disable debug logging in Jeeves.lua
 ]])
